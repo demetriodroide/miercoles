b001f2d904ff37ac84a917d898b8f9547d344d44
demetrio <demeuforia@gmail.com>
2020-10-20 23:03:56 +0200

    por terminar

---
+++ index.js
@@ -0,0 +1,91 @@
+let Coche = {
+marca : "asd",
+modelo : 2,
+matricula : "as44",
+}
+
+let Casa = {
+    codPostal : 1,
+    calle : "we",
+    portal : 2,
+    piso : "weew"
+}
+
+let FullStackDeveloper = {
+    lenguajes : [],
+    proyectos : [],
+}
+
+let Perro = {
+    nombre : "",
+    raza : "ogro",
+    color : "",
+    edad : 2,
+    ladrar() {
+        console.log("ladrido");
+        
+    },
+
+    popo(){
+return (Math.random()*3);
+    }
+
+}
+
+//console.log(Perro.raza);
+
+
+
+let marcaPortatil = Portatil.marca;
+//console.log(Portatil.marca);
+
+let marcaPortatil2 = Portatil["marca"];
+//console.log(Portatil["marca"])
+//console.log(Portatil);
+//console.log(marcaPortatil2);
+
+let grupos = Concierto.grupos;
+
+//console.log(Casa);
+
+let RGB = [Led.rojo, Led.verde, Led.azul];
+
+//
+
+Portatil.modelo = "P345";
+Concierto.cartelera.push("Guns N' Roses");
+Concierto.fecha = new Date();
+Impresora.imprimiendo = {
+    nombreArchivo : "",
+    copias : "",
+    numPaginas : "",
+}
+
+let Noticia = {
+    titular : "",
+    cuerpo : ""
+}
+
+let Persona = {
+    nombre : "",
+    apellidos : "",
+    edad : ""
+}
+
+let Avion = {
+    numPasajeros : "",
+    despegar(){
+        console.log("despegando");
+    },
+    volar(){
+        console.log("llegando al destino");
+    },
+    aterrizar(){
+        console.log("aterrizando");
+    }
+}
+
+let Paquete = {
+    contenido : ["","",""]
+}
+
